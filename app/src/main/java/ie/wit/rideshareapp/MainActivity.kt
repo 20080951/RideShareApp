242070d689c5f46165dbf079aec95b537123f0b8
20080951 <72085852+20080951@users.noreply.github.com>
2021-04-06 13:12:06 +0100

    Register
    Completed Toast Validation

--- app/src/main/java/ie/wit/rideshareapp/MainActivity.kt
+++ app/src/main/java/ie/wit/rideshareapp/MainActivity.kt
@@ -1,11 +1,23 @@
 package ie.wit.rideshareapp
-
+import kotlinx.android.synthetic.main.activity_main.*
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnRegister.setOnClickListener {
+            if (editEmail.text.trim().toString().isNotEmpty() || editPassword.text.trim().toString().isNotEmpty()) {
+
+
+            Toast.makeText(this, "Input Provided", Toast.LENGTH_LONG).show()
+        }else {
+            Toast.makeText(this, "Input Required", Toast.LENGTH_LONG).show()
+        }
+    }
     }
 }
