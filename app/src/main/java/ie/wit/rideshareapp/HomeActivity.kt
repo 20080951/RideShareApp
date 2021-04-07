c57cb0b99c7becdccf68f5c9c38c1c3370cb492c
20080951 <72085852+20080951@users.noreply.github.com>
2021-04-07 17:10:54 +0100

    Navbar
    Created NavBar;
    Created Fragments;
    Connected HomeActivity to HomeFragment

--- app/src/main/java/ie/wit/rideshareapp/HomeActivity.kt
+++ app/src/main/java/ie/wit/rideshareapp/HomeActivity.kt
@@ -3,10 +3,18 @@ package ie.wit.rideshareapp
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import com.google.firebase.auth.FirebaseAuth
+import ie.wit.rideshareapp.fragments.*
 import kotlinx.android.synthetic.main.activity_home.*
 
 class HomeActivity : AppCompatActivity() {
+
+    private val homeFragment = HomeFragment()
+    private val accountFragment = AccountFragment()
+    private val myridesFragment = MyridesFragment()
+    private val settingsFragment = SettingsFragment()
+    private val sharerideFragment = SharerideFragment()
 
     private lateinit var auth: FirebaseAuth
 
@@ -14,11 +22,33 @@ class HomeActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
         auth = FirebaseAuth.getInstance()
+        replaceFragment(homeFragment)
 
-        btnSignOut.setOnClickListener {
-            auth.signOut()
-            val intent = Intent(this, LoginActivity::class.java)
-            startActivity(intent)
+      //  btnSignOut.setOnClickListener {
+        //    auth.signOut()
+          //  val intent = Intent(this, LoginActivity::class.java)
+            //startActivity(intent)
+        //}
+    }
+
+    private fun replaceFragment(fragment: Fragment){
+        if(fragment !=null){
+        val transaction = supportFragmentManager.beginTransaction()
+            transaction.replace(R.id.fragment_container, fragment)
+            transaction.commit()
         }
     }
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
