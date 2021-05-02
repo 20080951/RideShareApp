34952094fd4366a3a2df59a513602623533a1aeb
20080951 <72085852+20080951@users.noreply.github.com>
2021-05-02 22:08:04 +0100

    Update Rides.kt

--- app/src/main/java/ie/wit/rideshareapp/classes/Rides.kt
+++ app/src/main/java/ie/wit/rideshareapp/classes/Rides.kt
@@ -1,4 +1,4 @@
 package ie.wit.rideshareapp.classes
-
+// we assign all the data in rides which will be used to populate our recycler view
 data class Rides(var pickupLocation : String ?= null, var destination : String ?= null,var date : String?= null, var contact : Long ?= null)
 
